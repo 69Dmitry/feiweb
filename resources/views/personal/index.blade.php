b1e93bec930f8729c959560cfaa5407442c8b33e
Дмитрий <Sem7sem7sem8@gmail.com>
2024-12-26 19:48:16 +0300

    styles update

--- resources/views/personal/index.blade.php
+++ resources/views/personal/index.blade.php
@@ -22,6 +22,36 @@
         </div>
     </div>
     <div class="col-9">
+        <h1 class="contacts__title">Заявки</h1>
+        <div class="tabs d-flex">
+            <div class="tabs__item">Активные</div>
+            <div class="tabs__item">Завершенные</div>
+        </div>
+
+        <div class="orders">
+            <div class="order">
+                <div class="order__top d-flex justify-content-between">
+                    <div class="order__date">№13546 от 01.11.2024</div>
+                    <div class="order__status">Активная</div>
+                </div>
+                <div class="order__info d-flex justify-content-between">
+                        <div class="order__item">
+                         <b>Сотрудник</b><br>
+                         <span>Головина Д.А.</span>
+                        </div>
+                        <div class="order__item">
+                         <b>Результат</b><br>
+                         <span>Обработка</span>
+                        </div>
+                        <div class="order__item">
+                         <b>Услуги</b><br>
+                         <span>Маркетинговая аналитика<br>
+                         Налоговый аудит</span>
+                        </div>
+                    
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </div>
