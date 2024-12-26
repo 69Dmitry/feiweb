d09d172b6bc49f969bffade4c9f66235c59a456e
Дмитрий <Sem7sem7sem8@gmail.com>
2024-12-26 19:29:12 +0300

    index controller fix

--- app/Http/Controllers/Personal/IndexController.php
+++ app/Http/Controllers/Personal/IndexController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Personal;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Tools\Helpers\MenuHelper;
 
 class IndexController extends Controller
 {
     //
     public function __invoke(Request $request)
     {
-        return view('personal.index');
+        $menu = MenuHelper::getMenuJsonFile();
+        return view('personal.index', compact('menu'));
     }
 }
