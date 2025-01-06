6530084e58fe1b660b086ff85c46a2b759bba64f
Дмитрий <Sem7sem7sem8@gmail.com>
2025-01-06 16:26:19 +0300

    update personal

--- resources/views/personal/view.blade.php
+++ resources/views/personal/view.blade.php
@@ -24,29 +24,37 @@
         <div class="col-9">
             <h1 class="contacts__title">Личная информация</h1>
             <div class="row">
-                <div class="col-lg-4">
+                <div class="col-lg-4 personal__card">
                     <span class="personal__label">ФИО </span>
                     <span class="personal__value"> Петров Сергей Иванович </span>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-lg-4 personal__card">
                     <span class="personal__label">Телефон </span>
                     <span class="personal__value"> +7 928 837 37 38 </span>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-lg-4 personal__card">
                 <span class="personal__label"> Статус</span>
                 <span class="personal__value"> Подтвержден </span>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-lg-4 personal__card">
                 <span class="personal__label"> Почта</span>
                 <span class="personal__value"> petrovSI@gmail.com </span>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-lg-4 personal__card">
                 <span class="personal__label"> Тип клиента</span>
                 <span class="personal__value"> Физическое лицо </span>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-lg-4 personal__card">
                 <span class="personal__label"> Пол </span>
                 <span class="personal__value"> Мужской </span>
+                </div>
+                <div class="col-lg-12 personal__card">
+                <span class="personal__label"> Персональный менеджер </span>
+                <span class="personal__value"> Ломова Габриэлла Геннадьевна </span>
+                </div>
+                <div class="col-lg-12 personal__card">
+                <span class="personal__label"> Договор </span>
+                <span class="personal__value"> Договор с Петровым Сергеем Ивановичем №12984-кл от 20.10.2024 </span>
                 </div>
 
             </div>
