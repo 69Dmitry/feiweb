5c5bfed907afb9f887aa00d40084b81c08b3a6e2
Дмитрий <Sem7sem7sem8@gmail.com>
2025-01-06 15:46:16 +0300

    view blade

---
+++ resources/views/personal/view.blade.php
@@ -0,0 +1,56 @@
+@include('parts.header')
+<div class="container">
+    <div class="row">
+        <ul class="breadcrumbs">
+            <li class="breadcrumbs__item"> <a class="breadcrumbs__link" href="">Главная / </a></li>
+            <li class="breadcrumbs__item breadcrumbs__item-active">Личный кабинет</li>
+        </ul>
+    </div>
+
+    <div class="row">
+        <div class="col-3">
+            <div class="aside">
+                <a href="#" class="aside__item aside__item_active">
+                    ЛИЧНАЯ ИНФОРМАЦИЯ
+                </a>
+                <a href="#" class="aside__item ">
+                    ЗАЯВКИ
+                </a>
+                <a href="#" class="aside__item">
+                    РЕДАКТИРОВАНИЕ ДАННЫХ
+                </a>
+            </div>
+        </div>
+        <div class="col-9">
+            <h1 class="contacts__title">Личная информация</h1>
+            <div class="row">
+                <div class="col-lg-4">
+                    <span class="personal__label">ФИО </span>
+                    <span class="personal__value"> Петров Сергей Иванович </span>
+                </div>
+                <div class="col-lg-4">
+                    <span class="personal__label">Телефон </span>
+                    <span class="personal__value"> +7 928 837 37 38 </span>
+                </div>
+                <div class="col-lg-4">
+                <span class="personal__label"> Статус</span>
+                <span class="personal__value"> Подтвержден </span>
+                </div>
+                <div class="col-lg-4">
+                <span class="personal__label"> Почта</span>
+                <span class="personal__value"> petrovSI@gmail.com </span>
+                </div>
+                <div class="col-lg-4">
+                <span class="personal__label"> Тип клиента</span>
+                <span class="personal__value"> Физическое лицо </span>
+                </div>
+                <div class="col-lg-4">
+                <span class="personal__label"> Пол </span>
+                <span class="personal__value"> Мужской </span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@include('parts.footer')
