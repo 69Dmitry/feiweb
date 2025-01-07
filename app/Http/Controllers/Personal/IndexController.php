e6813b77eaa4e9d00cb6e2c429f080f41d5b89c0
Дмитрий <Sem7sem7sem8@gmail.com>
2025-01-07 17:14:51 +0300

    controllers

--- app/Http/Controllers/Personal/IndexController.php
+++ app/Http/Controllers/Personal/IndexController.php
@@ -6,12 +6,14 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Tools\Helpers\MenuHelper;
 
+
 class IndexController extends Controller
 {
     //
     public function __invoke(Request $request)
     {
         $menu = MenuHelper::getMenuJsonFile();
+     
         return view('personal.index', compact('menu'));
     }
 }
--- app/Http/Controllers/Personal/ViewController.php
+++ app/Http/Controllers/Personal/ViewController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Personal;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Tools\Helpers\MenuHelper;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class ViewController extends Controller
 {
@@ -12,6 +14,13 @@ class ViewController extends Controller
     public function __invoke(Request $request)
     {
         $menu = MenuHelper::getMenuJsonFile();
-        return view('personal.view', compact('menu'));
+        $id = 1;
+        if (Auth::check() && Auth::id()) {
+            $id = Auth::id();
+        }
+      
+        $user = User::find($id)->firstorFail();
+
+        return view('personal.view', compact('menu', 'user'));
     }
 }
