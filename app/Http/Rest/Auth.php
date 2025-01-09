72822eb349eb55e93150481b9545f8f5f2021cbd
Дмитрий <Sem7sem7sem8@gmail.com>
2025-01-09 18:15:12 +0300

    auth

--- app/Http/Rest/Auth.php
+++ app/Http/Rest/Auth.php
@@ -4,8 +4,11 @@ namespace App\HTTP\Rest;
 
 class Auth
 {
+    protected const REMOTE_ADDRESSES = ['91.191.190.94'];
     public static function isAllowAccess()
     {
-        return true;
+        if (in_array($_SERVER['REMOTE_ADDR'], self::REMOTE_ADDRESSES)){ 
+            return true;
+        }
     }
 }
