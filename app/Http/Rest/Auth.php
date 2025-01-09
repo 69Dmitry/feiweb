995ae7e454ccccdf40986137b55c78cba724db04
Дмитрий <Sem7sem7sem8@gmail.com>
2025-01-09 18:20:18 +0300

    fix auth checker

--- app/Http/Rest/Auth.php
+++ app/Http/Rest/Auth.php
@@ -1,14 +1,20 @@
 <?php
 
 namespace App\HTTP\Rest;
-
+use Illuminate\Support\Facades\Request;
 class Auth
 {
+    /**
+     * IP сервера с 1с
+     */
     protected const REMOTE_ADDRESSES = ['91.191.190.94'];
+
     public static function isAllowAccess()
     {
-        if (in_array($_SERVER['REMOTE_ADDR'], self::REMOTE_ADDRESSES)){ 
+        if (in_array(Request::ip(), self::REMOTE_ADDRESSES)) {
             return true;
         }
+
+        return response()->json(['error' => 'Forbidden'], 403);
     }
 }
