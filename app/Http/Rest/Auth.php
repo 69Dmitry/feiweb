d810daae68544cefc13932800747a8b4d55cd6bd
Дмитрий <Sem7sem7sem8@gmail.com>
2025-01-12 13:49:25 +0300

    Auth fix

--- app/Http/Rest/Auth.php
+++ app/Http/Rest/Auth.php
@@ -8,10 +8,11 @@ class Auth
      * IP сервера с 1с
      */
     protected const REMOTE_ADDRESSES = ['91.191.190.94'];
+    protected const IS_DEBUG = true;
 
     public static function isAllowAccess()
     {
-        if (in_array(Request::ip(), self::REMOTE_ADDRESSES)) {
+        if (in_array(Request::ip(), self::REMOTE_ADDRESSES) || self::IS_DEBUG) {
             return true;
         }
 
