cc6c56cdeae0511750f3e81cf16043508d58d490
Дмитрий <Sem7sem7sem8@gmail.com>
2025-01-19 15:11:27 +0300

    fix

--- resources/views/service/show.blade.php
+++ resources/views/service/show.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1 class="m-0">Просмотр статьи «{{ $article->title }}»</h1>
+              <h1 class="m-0">Просмотр услуги «{{ $service->title }}»</h1>
             </div><!-- /.col -->
 
             <div class="col-sm-6 text-right">
