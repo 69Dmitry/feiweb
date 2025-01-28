e66d2b531148e67cc32f5de69a25a05f467f50c8
Дмитрий <Sem7sem7sem8@gmail.com>
2025-01-28 08:45:09 +0300

    fix

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -13,13 +13,22 @@
             <input class="form__input" name="q" placeholder="Введите запрос" value="{{$query}}">
             <button class="button button__form">Найти</button>
         </form>
+       
         @if ($counter > 0)
             @foreach ($searchResults as $searchResult)
+     
             <div class="col-lg-12">
-                <a class="search__item" href="">
+                @if ($searchResult instanceof \App\Models\Article)
+                <a class="search__item" href="{{route('article.view', ['slug' => $searchResult->slug ]) }}">
                     <div style="display: inline-block;" class="link__curly">{{$searchResult->title}}</div>
                     <p class="search__item-text">{{$searchResult->previewText}}</p>
                 </a>
+                @else
+                <a class="search__item" href="{{route('news.view', ['slug' => $searchResult->slug ]) }}">
+                    <div style="display: inline-block;" class="link__curly">{{$searchResult->title}}</div>
+                    <p class="search__item-text">{{$searchResult->previewText}}</p>
+                </a>
+                @endif
             </div>
             @endforeach
         @else
